85e83d7d3cecd5d1ceb3f0c91ba0ac5054e520a9
Genevieve Schultz <genschultz@gmail.com>
2023-10-31 13:21:17 -0700

    particle effects, screen shake, scroll bg, audio

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] int health = 50;
+    [SerializeField] ParticleSystem hitEffect;
+
+    [SerializeField] bool applyCameraShake;
+    CameraShake cameraShake;
+    AudioPlayer audioPlayer;
+
+    void Awake() {
+        cameraShake = Camera.main.GetComponent<CameraShake>();
+        audioPlayer = FindObjectOfType<AudioPlayer>();
+    }
 
     void OnTriggerEnter2D(Collider2D other) {
         DamageDealer damageDealer = other.GetComponent<DamageDealer>();
@@ -12,6 +22,9 @@ public class Health : MonoBehaviour
         if (damageDealer != null)
         {
             TakeDamage(damageDealer.GetDamage());
+            PlayHitEffect();
+            audioPlayer.PlayExplosionClip();
+            ShakeCamera();
             damageDealer.Hit();
         }
     }
@@ -22,6 +35,23 @@ public class Health : MonoBehaviour
         if (health <= 0)
         {
             Destroy(gameObject);
+        }
+    }
+
+    void PlayHitEffect()
+    {
+        if (hitEffect != null)
+        {
+            ParticleSystem instance = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(instance.gameObject, instance.main.duration + instance.main.startLifetime.constantMax);
+        }
+    }
+
+    void ShakeCamera()
+    {
+        if (cameraShake != null && applyCameraShake)
+        {
+            cameraShake.Play();
         }
     }
 }
